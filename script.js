5a13a391238c5e57c9ad092e24c3300d4f78523f
Daniil Andreev <daniil03022006dan@gmail.com>
2020-03-27 19:01:52 +0300

    2nd home work

---
+++ script.js
@@ -0,0 +1,16 @@
+let money = 100000;
+let income = 10000;
+let addExpenses = 'Internet, Taxi';
+let deposit = false;
+let mission = 1000000;
+let period = 9;
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let budgetDay = 50;
+console.log('budgetDay: ', budgetDay);
