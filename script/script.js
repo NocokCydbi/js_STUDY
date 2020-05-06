87b0edcdbc15d0a5ff0bcae48c55ff9653694a64
Daniil Andreev <daniil03022006dan@gmail.com>
2020-05-07 03:23:42 +0500

    lesson26

---
+++ script/script.js
@@ -0,0 +1,45 @@
+'use strict';
+const output = document.getElementById('output');
+
+const getData = (url) => {
+
+    return new Promise ((resolve, reject) => {
+        const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState !== 4){
+            return;
+        }
+        if(request.status === 200){
+            const response = JSON.parse(request.responseText);
+            resolve(response);
+        }else{
+            reject(request.statusText);
+        }
+    });
+    request.send();
+
+    });
+    
+};
+
+const outputPhotos = (data) => {
+    console.log(data);
+
+    data.forEach((item) => {
+    output.insertAdjacentHTML("beforebegin",
+    `<h4>${item.title}</h4>
+    <img src="${item.thumbnailUrl}" alt="${item.title}">`);
+    });
+    
+     
+};
+
+const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
+
+const oneImg = getData('https://jsonplaceholder.typicode.com/photos/1'),
+    twoIgm = getData('https://jsonplaceholder.typicode.com/photos/2');
+
+Promise.all([oneImg, twoIgm])
+    .then(outputPhotos)
+    .catch(error => console.log(error));
